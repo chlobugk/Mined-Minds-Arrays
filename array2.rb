54dbb6e447f41656630b1d0a75e6f60af53472b4
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-01-23 14:38:30 -0500

    mod 5

--- array2.rb
+++ array2.rb
@@ -6,8 +6,18 @@ def create_100_element_array
 
 	t100_element_array.each_with_index do |value,index|
 
-		if (value - 1) % 3 == 0
+		if value % 15 == 0
+			t100_element_array[index] = 'mined_minds'
+
+		elsif value % 3 == 0
 			t100_element_array[index] = 'mined'
+
+		elsif value % 5 == 0
+			t100_element_array[index] = 'minds'
+
+		else 
+			value
+			
 		end
 	end
 end
--- test_array2.rb
+++ test_array2.rb
@@ -13,6 +13,22 @@ class TestMMFunctions < Minitest::Test
 
 	def test_3_returns_mined
 		results = create_100_element_array
-		assert_equal('mined', results[3])
+		assert_equal('mined', results[3-1])
 	end
+
+	def test_1_returns_1
+		results = create_100_element_array
+		assert_equal(1, results[1-1])
+	end
+
+	def test_5_returns_minds
+		results = create_100_element_array
+		assert_equal('minds', results[5-1])
+	end
+
 end
+
+
+
+
+
